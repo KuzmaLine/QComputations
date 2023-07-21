837205826b53ea49ac6212d0004ec4ffdec56775
KuzmaLine <andrey200202@yandex.ru>
2023-07-21 10:00:24 +0300

    diploma

--- src/plot.hpp
+++ src/plot.hpp
@@ -1,2 +1,9 @@
 #pragma once
 #include "matplotlibcpp.h"
+#include "dynamic.hpp"
+#include "state.hpp"
+#include <algorithm>
+
+void probs_to_plot(const Evolution::Probs& probs, std::vector<double> time_vec, std::set<State> basis) {
+    for (const auto& )
+}
