c968f8a5907e53334f39792ae20d00b9a302b1ce
Kuzminskiy Andrey <s02200417@gse.cs.msu.ru>
2023-09-01 18:48:11 +0300

    cleaning

--- test.py
+++
@@ -1,6 +0,0 @@
-x = 1
-i = 1
-while True:
-    x *= i
-    i += 1
-    #print(x)
--- to_lomonosov.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-scp -i ~/.ssh/mgu -r ~/Quantum/diploma/src/* s02200417_2309@lomonosov2.parallel.ru:~/_scratch/diploma/src/.
-ssh lomonosov
