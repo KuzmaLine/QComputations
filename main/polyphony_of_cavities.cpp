af263a45a7d2fa7a8abed94d4e47b7491d983891
Kuzminskiy Andrey <s02200417@gse.cs.msu.ru>
2023-10-20 11:43:46 +0300

    polyphony

--- main/polyphony_of_cavities.cpp
+++ main/polyphony_of_cavities.cpp
@@ -11,13 +11,14 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     QConfig::instance().set_width(20);
+    QConfig::instance().set_g(0.001);
 
-    std::vector<size_t> grid_config = {1, 0, 0, 1};
+    std::vector<size_t> grid_config = {1, 0, 1};
     State grid(grid_config);
     //grid.reshape(2, 2, 1);
     grid.set_max_N(3);
     grid.set_min_N(3);
-    grid.set_waveguide(0.004, 1);
+    grid.set_waveguide(0.002 / sqrt(10), 1);
     grid.set_qubit(0, 0, 1);
     //grid.set_n(1, 0);
     //grid.set_qubit(1, 0, 1);
@@ -33,7 +34,7 @@ int main(int argc, char** argv) {
     std::vector<COMPLEX> init_state(H.size(), 0);
     init_state[grid.get_index(H.get_basis())] = COMPLEX(1, 0);
 
-    auto time_vec = linspace(0, 8000, 16000);
+    auto time_vec = linspace(0, 32000, 64000);
 
     auto probs = Evolution::quantum_master_equation(init_state, H, time_vec);
 
