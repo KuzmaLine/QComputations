ee0170e2430ff39d8ab0d1395de87e8bf9572a57
Kuzminskiy Andrey <s02200417@gse.cs.msu.ru>
2023-10-21 17:25:48 +0300

    CSV ADD

--- main/polyphony_of_cavities.cpp
+++ main/polyphony_of_cavities.cpp
@@ -10,33 +10,73 @@ int main(int argc, char** argv) {
     int rank, world_size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    QConfig::instance().set_width(20);
-    QConfig::instance().set_g(0.001);
+    QConfig::instance().set_width(30);
+    //QConfig::instance().set_g(0.001);
 
-    std::vector<size_t> grid_config = {1, 0, 1};
+    std::vector<size_t> grid_config = {3, 3};
     State grid(grid_config);
-    //grid.reshape(2, 2, 1);
-    grid.set_max_N(3);
-    grid.set_min_N(3);
-    grid.set_waveguide(0.002 / sqrt(10), 1);
+    //grid.reshape(2, 1, 1);
+    grid.set_waveguide(0.0015, 1);
+    //grid.set_waveguide(0, 1);
     grid.set_qubit(0, 0, 1);
+    grid.set_qubit(0, 1, 1);
+    grid.set_qubit(0, 2, 1);
     //grid.set_n(1, 0);
     //grid.set_qubit(1, 0, 1);
+
+    State grid_copy(grid);
+    grid_copy.set_qubit(0, 0, 0);
+    grid_copy.set_qubit(0, 1, 0);
+    grid_copy.set_qubit(0, 2, 0);
+    grid_copy.set_qubit(grid_config.size() - 1, 0, 1);
+    grid_copy.set_qubit(grid_config.size() - 1, 1, 1);
+    grid_copy.set_qubit(grid_config.size() - 1, 2, 1);
 
     int ctxt;
     mpi::init_grid(ctxt);
     BLOCKED_H_TCH H(ctxt, grid);
+
+    //H.get_blocked_matrix().write_to_csv_file("H.csv");
 
     if (rank == 0) { show_basis(H.get_basis()); }
 
     H.show();
 
     std::vector<COMPLEX> init_state(H.size(), 0);
+    //std::vector<COMPLEX> target_state(H.size(), 0);
     init_state[grid.get_index(H.get_basis())] = COMPLEX(1, 0);
+    size_t target_index = grid_copy.get_index(H.get_basis());
+    //target_state[grid_copy.get_index(H.get_basis())] = COMPLEX(1, 0);
 
-    auto time_vec = linspace(0, 32000, 64000);
+    auto time_vec = linspace(0, 10000, 15000);
 
     auto probs = Evolution::quantum_master_equation(init_state, H, time_vec);
+    
+    double max_prob = 0;
+    size_t t_max = 0;
+    QConfig::instance().set_eps(1e-1);
+    for (size_t t = 0; t < time_vec.size(); t++) {
+        auto prob = probs.get(target_index, t);
+        //if (rank == mpi::ROOT_ID and time_vec[t] >= 9300 and time_vec[t] <= 9450) {
+        //    usleep(1000);
+        //    std::cout << t << " - " << prob << std::endl;
+        //}
+        if (is_zero(prob - 1)) {
+            if (prob > max_prob) {
+                max_prob = std::max(prob, max_prob);
+                t_max = t;
+            }
+        } else {
+            if (max_prob > 0) {
+                if (rank == 0) {
+                    std::cout << "Prob - " << max_prob << " | " << "t = " << time_vec[t] << " | IN PI - " << time_vec[t] / M_PI << " * PI " << std::endl; 
+                }
+                
+                max_prob = 0;
+                t_max = 0;
+            }
+        }
+    }
 
     if (rank == 0) {
         matplotlib::make_figure(1920, 1080);
