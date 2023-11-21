1209787232b34a5c46a928d5d5bed670169b8536
Kuzminskiy Andrey <s02200417@gse.cs.msu.ru>
2023-11-21 13:24:13 +0300

    regex

--- examples/schrodinger.cpp
+++ examples/schrodinger.cpp
@@ -5,29 +5,135 @@
 
 using COMPLEX = std::complex<double>;
 
-std::string make_state_regex_pattern(const std::string& format, bool is_freq_display, bool is_sequence) {
+
+// not ready
+std::string make_state_regex_pattern(const std::string& format, const std::string& del, bool is_sequence) {
+    using namespace QComputations;
     std::string res;
-    std::regex format_regex("(\\$[N,W,\\!,M])");
+    std::regex format_regex("(\\$\\!)");
 
     auto regex_begin = std::sregex_iterator(format.begin(), format.end(), format_regex);
     auto regex_end = std::sregex_iterator();
 
-    std::smatch match;
-    bool is_first = true;
-    for (std::sregex_iterator i = regex_begin; i != regex_end; ++i) {
-        match = *i;
-        if (match.str() == PHOTONS_STR) {
-            if (is_first) {
-                is_first = false;
-                continue;
-            }
-        }
-        std::cout << match.str(0) << " # " << match.prefix() << std::endl;
+    std::string delim = del;
+    if (del == ",") {
+        delim = "\\,";
     }
 
-    std::cout << match.suffix() << std::endl;
+    assert(regex_begin != regex_end); // В формате должен быть разделитель - "$!"
 
-    return regex_begin->str();
+    std::smatch match = *regex_begin;
+    std::string first_part = match.prefix();
+    std::string second_part = match.suffix();
+
+    format_regex = std::regex("(\\$[N,W])");
+
+    regex_begin = std::sregex_iterator(first_part.begin(), first_part.end(), format_regex);
+    regex_end = std::sregex_iterator();
+
+    std::string left_part;
+    std::sregex_iterator word = regex_begin;
+    match = *word;
+
+    if (match.str() == "$N") {
+        for (const auto c: std::string(match.prefix())) {
+            if (c == '|' or c == '_') left_part += "\\";
+            left_part += c;
+        }
+
+        left_part += "(\\d";
+        ++word;
+        if (word != regex_end) {
+            match = *word;
+            left_part += "([";
+            for (const auto c: std::string(match.prefix())) {
+                if (c == '|' or c == '_') left_part += "\\";
+                left_part += c;
+            }
+            left_part += "][\\d]{2})?";
+            left_part += match.suffix();
+        } else {
+            left_part += match.suffix();
+        }
+    } else if (match.str() == "$W") {
+        left_part += "[";
+        left_part += match.prefix();
+        left_part += "\\d\\d]?";
+        ++word;
+        if (word != regex_end) {
+            match = *word;
+            left_part += match.prefix();
+            left_part += "\\d+";
+            left_part += match.suffix();
+        } else {
+            left_part += match.suffix();
+        }
+    } else if (match.str() == "$M") {
+        left_part += match.prefix();
+        left_part += "[0-9]+";
+    } else {
+        std::cerr << "FORMAT ERROR ON FIRST PART!" << std::endl;
+    }
+
+    format_regex = std::regex("(\\$M)");
+
+    regex_begin = std::sregex_iterator(second_part.begin(), second_part.end(), format_regex);
+    std::string right_part;
+
+    match = *regex_begin;
+
+    if (match.str() == "$N") {
+        right_part += match.prefix();
+        right_part += "\\d+";
+        ++word;
+        if (word != regex_end) {
+            match = *word;
+            right_part += "[";
+            right_part += match.prefix();
+            right_part += "\\d\\d]?";
+            right_part += match.suffix();
+        } else {
+            right_part += match.suffix();
+        }
+    } else if (match.str() == "$W") {
+        right_part += "[";
+        right_part += match.prefix();
+        right_part += "\\d\\d]?";
+        ++word;
+        if (word != regex_end) {
+            match = *word;
+            right_part += match.prefix();
+            right_part += "\\d+";
+            right_part += match.suffix();
+        } else {
+            right_part += match.suffix();
+        }
+    } else if (match.str() == "$M") {
+        right_part += "[";
+        right_part += match.prefix();
+        right_part += "]?";
+        if (!is_sequence) {
+            right_part += "(";
+        }
+        right_part += "[0-9]*";
+
+        if (!is_sequence) {
+            right_part += "[" + delim + "]?";
+            right_part += ")*";
+        }
+
+        right_part += match.suffix();
+    } else {
+        std::cerr << "FORMAT ERROR ON SECOND PART!" << std::endl;
+    }
+
+    if (is_sequence) {
+        res = "((" + left_part + "[" + delim + "]?)+" + right_part + ")+)";
+    } else {
+        res = "(" + left_part + "[" + delim + "]?)+" + right_part + ")"; 
+    }
+
+    return res;
 }
 
 int main(int argc, char** argv) {
@@ -68,8 +174,24 @@ int main(int argc, char** argv) {
 
     auto time_vec = linspace(0, 1000, 2000);
 
+    QConfig::instance().set_state_format("|$N$!;$M>");
     std::cout << "0\u2082\u2083\u29FD" << std::endl;
-    std::cout << make_state_regex_pattern(QConfig::instance().state_format(), true, false) << std::endl;
+    std::cout << QConfig::instance().state_format() << std::endl;
+    std::string pattern = make_state_regex_pattern(QConfig::instance().state_format(), ",", false);
+    std::cout << pattern << std::endl;
+    std::regex reg(pattern);
+    std::regex test_reg("\\|");
+
+    std::string expr = "1+|0,12,13;001,012>+|0>+|0_01,1_12;112>";
+    //std::string expr = "1+|0;001>|12_01,12_02;112>+|12>|12;001> + |12>";
+    std::cout << expr << std::endl;
+    auto regex_begin = std::sregex_iterator(expr.begin(), expr.end(), reg);
+    auto regex_end = std::sregex_iterator();
+
+    for (auto i = regex_begin; i != regex_end; i++) {
+        std::smatch match = *i;
+        std::cout << match.prefix() << " " << match.str() << std::endl;
+    }
 
     MPI_Finalize();
     return 0;
