9c1e166889f54aea4986ceab11f682b25736e032
Kuzminskiy Andrey <s02200417@gse.cs.msu.ru>
2023-12-03 14:46:54 +0300

    new_example

--- examples/graph_state.cpp
+++ examples/graph_state.cpp
@@ -7,22 +7,22 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    size_t cavity_counts = 3;
-    std::vector<size_t> grid_atoms;
-    std::pair<double, double> gamma = std::make_pair(0.01, 1);
-    std::pair<double, double> pair_zero = std::make_pair(0, 0);
-    Matrix<std::pair<double, double>> waveguides_parametrs({{pair_zero, gamma, pair_zero}, {gamma, pair_zero, gamma}, {pair_zero, gamma, pair_zero}});
+    size_t cavity_counts = 3; // число полостей
+    std::vector<size_t> grid_atoms; // задаёт количество частиц в каждой полости, у нас везде будут 0
+    std::pair<double, double> gamma = std::make_pair(0.01, 1); // амплитуда перехода из полости в полости
+    std::pair<double, double> pair_zero = std::make_pair(0, 0); // длина волновода
+    Matrix<std::pair<double, double>> waveguides_parametrs({{pair_zero, gamma, pair_zero}, {gamma, pair_zero, gamma}, {pair_zero, gamma, pair_zero}}); // матрица параметров
     for (size_t i = 0; i < cavity_counts; i++){
         grid_atoms.emplace_back(0);
     }
 
     State state(grid_atoms);
     state.set_waveguide(waveguides_parametrs);
-    state.set_leak_for_cavity(2, 0.001);
-    state.set_n(1, 0);
+    state.set_leak_for_cavity(2, 0.001); // для 2 полости делаем утечки 0.001
+    state.set_n(1, 0); // в 0 полость помещаем 1 фотон
 
     int ctxt;
-    mpi::init_grid(ctxt);
+    mpi::init_grid(ctxt); // делаем сетку процессов
     BLOCKED_H_TCH H(ctxt, state);
 
     if (rank == 0) show_basis(H.get_basis());
