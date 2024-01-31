c5107d21ca35c0c471810dae3e710f1766800ccf
Kuzminskiy Andrey <s02200417@gse.cs.msu.ru>
2024-01-31 15:42:59 +0300

    seaborn

--- install.sh
+++ install.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/bash
 
-./install_headers.sh /home/s02200417_2309/lib_result
+./install_headers.sh $1
+
+# Read the .bashrc file into a variable
+bashrc_content=$(cat ~/.bashrc)
+
+# Check if the alias already exists in .bashrc
+if echo "$bashrc_content" | grep -q "export SEABORN_PLOT"; then
+    # The alias exists, so remove it and add the new one
+    echo "$bashrc_content" | sed -i "s@export SEABORN_PLOT.*@export SEABORN_PLOT=\"$1\/QComputations\/seaborn_plot.py\"@" ~/.bashrc
+else
+    # The alias doesn't exist, so add it to .bashrc
+    echo -e "\nexport SEABORN_PLOT=\"$1/QComputations/seaborn_plot.py\"" >> ~/.bashrc
+fi
+
+sudo chmod +x $1/QComputations/seaborn_plot.py
+
 cd cpu_cluster
 cmake -DCMAKE_CXX_COMPILER=mpiicpc -B .
 make -j4 -B
-mv ./*.so /home/s02200417_2309/lib_result
-
+sudo mv ./*.so $2/.
 
 #cd ../single
 #cmake -DCMAKE_CXX_COMPILER=icpx -B .
 #make -j4 -B
 #sudo mv ./*.so $2/.
+
