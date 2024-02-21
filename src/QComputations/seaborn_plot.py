b9f9515e04c290a2bdb28e40e97ea2e604e9e74b
Kuzminskiy Andrey <s02200417@gse.cs.msu.ru>
2024-02-21 13:19:40 +0300

    ads

--- src/QComputations/seaborn_plot.py
+++ src/QComputations/seaborn_plot.py
@@ -1,5 +1,6 @@
 #!/opt/intel/oneapi/intelpython/latest/bin/python
 
+import os
 import json
 import pandas as pd
 import seaborn as sns
@@ -9,35 +10,61 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-json_file = "$SEABORN_CONFIG"
+def read_files(dir):
+    time_vec = pd.read_csv("./" + dir + "/time.csv", header=None).to_numpy().squeeze().tolist()
+    basis = pd.read_csv("./" + dir + "/basis.csv", header=None).to_numpy().squeeze().tolist()
+    probs = pd.read_csv("./" + dir + "/probs.csv", header=None)
+
+    print("TIME - ", len(time_vec))
+    print("Probs - ", probs.shape[0], probs.shape[1])
+
+    probs.index=time_vec
+    probs.columns = basis    
+
+    return probs
+
+json_file = os.environ.get('SEABORN_CONFIG')
 
 with open(json_file) as json_data:
     config = json.load(json_data)
 
-dir = config.get("dir")
-plotname = config.get("filename")
+format = config.get("format")
+if (format == "gif"):
+    dirs = config.get("dirs")
+    plotname = config.get("filename")
 
-# hamiltonian = pandas.read_csv("./" + dir + "/hamiltonian.csv", header=None)
-time_vec = pd.read_csv("./" + dir + "/time.csv", header=None).to_numpy().squeeze().tolist()
-basis = pd.read_csv("./" + dir + "/basis.csv", header=None).to_numpy().squeeze().tolist()
-probs = pd.read_csv("./" + dir + "/probs.csv", header=None)
+    # hamiltonian = pandas.read_csv("./" + dir + "/hamiltonian.csv", header=None)
+    time_vec = pd.read_csv("./" + dir + "/time.csv", header=None).to_numpy().squeeze().tolist()
+    basis = pd.read_csv("./" + dir + "/basis.csv", header=None).to_numpy().squeeze().tolist()
+    probs = pd.read_csv("./" + dir + "/probs.csv", header=None)
 
-print("TIME - ", len(time_vec))
-print("Probs - ", probs.shape[0], probs.shape[1])
+    print("TIME - ", len(time_vec))
+    print("Probs - ", probs.shape[0], probs.shape[1])
 
-probs.index=time_vec
-probs.columns = basis
+    probs.index=time_vec
+    probs.columns = basis
 
-print(probs)
+    print(probs)
 
-# print(hamiltonian)
-# print(hamiltonian.head(1))
-# print(time_vec)
-# print(basis)
+    # print(hamiltonian)
+    # print(hamiltonian.head(1))
+    # print(time_vec)
+    # print(basis)
 
-plt.figure(figsize = (int(config.get("width")), int(config.get("height"))))
-#plt.figure()
-sns.lineplot(data=probs)
-plt.grid()
-plt.savefig(plotname, format=config.get("format"))
-plt.show()
+    plt.figure(figsize = (int(config.get("width")), int(config.get("height"))))
+    #plt.figure()
+    sns.lineplot(data=probs)
+    plt.grid()
+    plt.savefig(plotname, format=config.get("format"))
+    plt.show()
+else:
+    dir = config.get("dir")
+    plotname = config.get("filename")
+
+    probs = read_files(dir)
+
+    plt.figure(figsize = (int(config.get("width")), int(config.get("height"))))
+    sns.lineplot(data=probs)
+    plt.grid()
+    plt.savefig(plotname, format=config.get("format"))
+    plt.show()
