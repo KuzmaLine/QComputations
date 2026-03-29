26a1633ea2c42f5e4e082b6e195e616d3b9b5ce2
AndrewOvvv <s21v_ovchinnikov@179.ru>
2026-03-29 19:52:31 +0000

    Refactor CSR_Matrix to use std::unique_ptr for internal arrays and move back .clang-format

--- src/QComputations/csr_matrix.hpp
+++ src/QComputations/csr_matrix.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <complex>
 #include <vector>
+#include <memory>
 #include "matrix.hpp"
 
 #include <functional>
@@ -12,8 +13,6 @@
 #include <mkl_spblas.h>
 #include <mkl_sparse_handle.h>
 
-// rewrite to return results of optimized_multiply
-
 namespace QComputations {
 
 namespace {
@@ -22,43 +21,8 @@ namespace {
         return (std::abs(a - b) < QConfig::instance().eps());
     }
 
-    ILP_TYPE get_index_value(ILP_TYPE row_index, const std::vector<ILP_TYPE>& ia) {
-        ILP_TYPE index_value = 0;
-        for (ILP_TYPE k = 0; k < row_index; k++) {
-            index_value = ia[k + 1];
-        }
-
-        return index_value;
-    }
-
-    template <typename T>
-    void insert_value_csr(ILP_TYPE i, ILP_TYPE j, T value,
-                          std::vector<T>& values, std::vector<ILP_TYPE>& ja,
-                          std::vector<ILP_TYPE>& ia) {
-        auto index_value = get_index_value(i, ia);
-        
-        //std::cout << index_value << std::endl;
-        if (index_value == values.size()) {
-            values.emplace_back(value);
-            ja.emplace_back(j);
-        } else {
-            for (ILP_TYPE k = 0; k < ia[i + 1] - ia[i]; k++) {
-                if (ja[index_value] < j) {
-                    index_value++;
-                } else {
-                    break;
-                }
-            }
-
-            values.insert(std::next(values.begin(), index_value), value);
-            ja.insert(std::next(ja.begin(), index_value), j);
-        }
-
-        //std::cout << "HERE\n";
-
-        for (ILP_TYPE k = i + 1; k < ia.size(); k++) {
-            ia[k]++;
-        }
+    ILP_TYPE get_index_value(ILP_TYPE row_index, const MKL_INT* ia) {
+        return ia[row_index];
     }
 }
 
@@ -71,12 +35,13 @@ class CSR_Matrix {
         explicit CSR_Matrix(const Matrix<T>& A, T default_value = T(0));
         explicit CSR_Matrix(std::function<T(ILP_TYPE, ILP_TYPE)> func, T default_value = T(0));
         explicit CSR_Matrix(const CSR_Matrix<T>& A);
-        explicit CSR_Matrix(const std::vector<T>& vals, const std::vector<ILP_TYPE>& ia, const std::vector<ILP_TYPE>& ja);
-        explicit CSR_Matrix(const std::function<void(std::vector<ILP_TYPE>& ia, std::vector<ILP_TYPE>& ja, std::vector<T>& vals)>&, T default_value = T(0));
-
+        explicit CSR_Matrix(ILP_TYPE n, ILP_TYPE m, const std::vector<T>& vals, const std::vector<MKL_INT>& ia, const std::vector<MKL_INT>& ja);
+        explicit CSR_Matrix(const std::function<void(std::vector<MKL_INT>& ia, std::vector<MKL_INT>& ja, std::vector<T>& vals)>&, T default_value = T(0));
 
         ~CSR_Matrix() {
-            mkl_sparse_destroy(mkl_matrix_);
+            if (mkl_matrix_ != nullptr) {
+                mkl_sparse_destroy(mkl_matrix_);
+            }
         }
 
         void insert_value(ILP_TYPE i, ILP_TYPE j, T value);
@@ -93,13 +58,16 @@ class CSR_Matrix {
         const std::vector<T> operator*(const std::vector<T>& x) const;
 
         void show() const;
+
+        sparse_matrix_t mkl_matrix() const { return mkl_matrix_; }
     private:
         bool is_exported_ = true;
-        ILP_TYPE n_;
-        ILP_TYPE m_;
-        T* vals_;
-        MKL_INT* ia_;
-        MKL_INT* ja_;
+        ILP_TYPE n_ = 0;
+        ILP_TYPE m_ = 0;
+        T default_value_ = T(0);
+        std::unique_ptr<T[]> vals_;
+        std::unique_ptr<MKL_INT[]> ia_;
+        std::unique_ptr<MKL_INT[]> ja_;
         sparse_matrix_t mkl_matrix_ = nullptr;
         // TEMP
         sparse_matrix_type_t matrix_type_ = SPARSE_MATRIX_TYPE_GENERAL;
@@ -108,33 +76,71 @@ class CSR_Matrix {
 
 // NAIVE VERSION for not double and complex double
 template<typename T>
-CSR_Matrix<T>::CSR_Matrix(const Matrix<T>& A, T default_value) : n_(A.n()), m_(A.m()), ia_(n_ + 1, 0), default_value_(default_value) {
+CSR_Matrix<T>::CSR_Matrix(const Matrix<T>& A, T default_value) : n_(A.n()), m_(A.m()), default_value_(default_value) {
+    std::vector<MKL_INT> ia_vec(n_ + 1, 0);
+    std::vector<MKL_INT> ja_vec;
+    std::vector<T> vals_vec;
+    
     for (ILP_TYPE i = 0; i < n_; i++) {
+        ia_vec[i + 1] = ia_vec[i];
         for (ILP_TYPE j = 0; j < m_; j++) {
             if (!is_close(A.elem(i, j), default_value)) {
-                //std::cout << "NEW_ELEM: " << i << " " << j << " " << A.elem(i, j) << std::endl;
-                //std::cout << vals_ << std::endl << ja_ << std::endl << ia_ << std::endl;
-                insert_value_csr(i, j, A.elem(i, j), vals_, ja_, ia_);
+                ia_vec[i + 1]++;
+                vals_vec.emplace_back(A.elem(i, j));
+                ja_vec.emplace_back(j);
             }
         }
+    }
+    
+    ia_ = std::make_unique<MKL_INT[]>(n_ + 1);
+    std::copy(ia_vec.begin(), ia_vec.end(), ia_.get());
+    
+    if (!ja_vec.empty()) {
+        ja_ = std::make_unique<MKL_INT[]>(ja_vec.size());
+        std::copy(ja_vec.begin(), ja_vec.end(), ja_.get());
+        
+        vals_ = std::make_unique<T[]>(vals_vec.size());
+        std::copy(vals_vec.begin(), vals_vec.end(), vals_.get());
     }
 }
 
 template<typename T>
-CSR_Matrix<T>::CSR_Matrix(ILP_TYPE n, ILP_TYPE m, const std::vector<T>& vals, const std::vector<ILP_TYPE>& ia, const std::vector<ILP_TYPE>& ja): n_(n), m_(m), vals_(vals), ia_(ia), ja_(ja), default_value_(default_value) {}
+CSR_Matrix<T>::CSR_Matrix(ILP_TYPE n, ILP_TYPE m, const std::vector<T>& vals, const std::vector<MKL_INT>& ia, const std::vector<MKL_INT>& ja): n_(n), m_(m) {
+    ia_ = std::make_unique<MKL_INT[]>(n_ + 1);
+    std::copy(ia.begin(), ia.end(), ia_.get());
+    
+    if (!ja.empty()) {
+        ja_ = std::make_unique<MKL_INT[]>(ja.size());
+        std::copy(ja.begin(), ja.end(), ja_.get());
+        
+        vals_ = std::make_unique<T[]>(vals.size());
+        std::copy(vals.begin(), vals.end(), vals_.get());
+    }
+}
 
 template<typename T>
-CSR_Matrix<T>::CSR_Matrix(const CSR_Matrix<T>& A): n_(A.n_), m_(A.m_), ia_(A.ia_), ja_(A.ja_), default_value_(A.default_value_) {}
+CSR_Matrix<T>::CSR_Matrix(const CSR_Matrix<T>& A): n_(A.n_), m_(A.m_), default_value_(A.default_value_) {
+    ia_ = std::make_unique<MKL_INT[]>(n_ + 1);
+    std::copy(A.ia_.get(), A.ia_.get() + n_ + 1, ia_.get());
+    
+    size_t nnz = A.ia_[n_];
+    if (nnz > 0) {
+        ja_ = std::make_unique<MKL_INT[]>(nnz);
+        std::copy(A.ja_.get(), A.ja_.get() + nnz, ja_.get());
+        
+        vals_ = std::make_unique<T[]>(nnz);
+        std::copy(A.vals_.get(), A.vals_.get() + nnz, vals_.get());
+    }
+}
 
 template<typename T>
 const T CSR_Matrix<T>::operator()(ILP_TYPE i, ILP_TYPE j) const {
-    ILP_TYPE index_value = get_index_value(i, ia_);
+    ILP_TYPE index_value = ia_[i];
 
     for (ILP_TYPE k = 0; k < ia_[i + 1] - ia_[i]; k++) {
         if (ja_[index_value] == j) { 
             return vals_[index_value];
         }
-
         index_value++;
     }
 
@@ -162,19 +168,19 @@ template<typename T>
 void optimized_multiply(const CSR_Matrix<T>& A, const CSR_Matrix<T>& B, CSR_Matrix<T>& C, char op = 'N');
 
 template<>
-void optimized_multiply(const CSR_Matrix<double>& A, const CSR_Matrix<double>& B, CSR_Matrix<double>& C, char op = 'N');
+void optimized_multiply(const CSR_Matrix<double>& A, const CSR_Matrix<double>& B, CSR_Matrix<double>& C, char op);
 
 template<>
-void optimized_multiply(const CSR_Matrix<COMPLEX>& A, const CSR_Matrix<COMPLEX>& B, CSR_Matrix<COMPLEX>& C, char op = 'N');
+void optimized_multiply(const CSR_Matrix<COMPLEX>& A, const CSR_Matrix<COMPLEX>& B, CSR_Matrix<COMPLEX>& C, char op);
 
 template<typename T>
 void optimized_multiply(const CSR_Matrix<T>& A, const Matrix<T>& B, Matrix<T>& C, T alpha = T(1), T betta = T(0), char op = 'N');
 
 template<>
-void optimized_multiply(const CSR_Matrix<double>& A, const Matrix<double>& B, Matrix<double>& C, double alpha = 1, double betta = 0,  char op = 'N');
+void optimized_multiply(const CSR_Matrix<double>& A, const Matrix<double>& B, Matrix<double>& C, double alpha, double betta, char op);
 
 template<>
-void optimized_multiply(const CSR_Matrix<COMPLEX>& A, const Matrix<COMPLEX>& B, Matrix<COMPLEX>& C, COMPLEX alpha = COMPLEX(1, 0), COMPLEX betta = COMPLEX(0, 0),  char op = 'N');
+void optimized_multiply(const CSR_Matrix<COMPLEX>& A, const Matrix<COMPLEX>& B, Matrix<COMPLEX>& C, COMPLEX alpha, COMPLEX betta, char op);
 
 template<typename T>
 void optimized_add(const CSR_Matrix<T>& A, const CSR_Matrix<T>& B, CSR_Matrix<T>& C);
